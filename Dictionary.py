e3cb42ea9d86583448e0cb5535e1ae7260e4c963
sahilwagh24 <44223996+sahilwagh24@users.noreply.github.com>
2018-12-04 13:14:10 +0530

    Create Dictionary.py

---
+++ Dictionary.py
@@ -0,0 +1,19 @@
+# sahil wagh
+# div m roll no 65
+#gr no :11810821
+
+def exponential (p,z):
+
+ f={z:z**p}
+
+ print(f)
+
+ return(f)
+
+while 1:
+
+ z=int(input())
+
+ p=int(input())
+
+ a=exponential (p,z)
